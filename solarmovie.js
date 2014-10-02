810c8cbcbf5f8aa0f2fa8dfba8f8e79326569bcb
mkeedlinger <mkeedlinger@gmail.com>
2014-10-02 09:13:42 -0700

    added solarmovie.js

---
+++ solarmovie.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name solarmovieEnhancer
+// @namespace https://github.com/mkeedlinger/userscripts
+// @author mkeedlinger
+// @version 0.1.0
+// @description Some nice touches to solarmovie
+// @updateURL   https://monkeyguts.com/104.meta.js?c
+// @downloadURL https://monkeyguts.com/104.user.js?c
+// @include        http://*.twitch.tv/*
+// @include        http://twitch.tv/*
+// @exclude        http://www.twitch.tv/*/chat?popout=
+// @exclude        http://www.twitch.tv/*/popout
+// @exclude        http://www.twitch.tv/*/dashboard
+// @exclude        http://www.twitch.tv/inbox*
+// @exclude        http://www.twitch.tv/subscriptions*
+// @exclude        http://store.twitch.tv
+// @exclude        http://api.twitch.tv/*
+// @exclude        https://api.twitch.tv/*
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js
+// @grant       GM_addStyle
+// @copyright  2014  tracerman
+// @icon        https://monkeyguts.com//icon/104.png
+// @run-at      document-end
+// ==/UserScript==
