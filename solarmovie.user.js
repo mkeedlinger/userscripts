a4bdd4de99cbb0c3dad94db1a231c9183c6abc90
mkeedlinger <mkeedlinger@gmail.com>
2014-10-04 14:41:02 -0700

    added fullscreen iframe insertion

--- solarmovie.user.js
+++ solarmovie.user.js
@@ -8,7 +8,7 @@
 // @source https://github.com/mkeedlinger/userscripts/blob/master/solarmovie.user.js
 // @updateURL https://github.com/mkeedlinger/userscripts/raw/master/solarmovie.user.js
 // @downloadURL https://github.com/mkeedlinger/userscripts/raw/master/solarmovie.user.js
-// @include http*://*.solarmovie.is/*
+// @include *
 // @require https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js
 // @run-at document-end
 // @grant GM_addStyle
@@ -29,74 +29,86 @@ var l = {
         get: GM_getValue,
         list: GM_listValues,
         addListener: GM_addValueChangeListener
-    };
+    },
+    secret = "Pp0DNfNApQSFMXGj1P7OswRj3YKJahOi09uwHyDOTgphSTSfMtmMWO16zgAAu2hCNzjPikGwx",
+    qs = document.querySelector;
 
-if ($('.dataTable').length) { // on a links page
-    console.log(1);
-    setTimeout(function () {
-        $('.commentsTree').remove();
-    }, 750);
-    $('.showmoreCell a').click();
-    GM_addStyle('.dataTable tbody .oddCell {background-color:rgba(231,239,243,.5)}');
+if ((new URL(window.location.href).hostname) === "www.solarmovie.is") {
+    if ($('.dataTable').length) { // on a links page
+        console.log(1);
+        setTimeout(function () {
+            $('.commentsTree').remove();
+        }, 750);
+        $('.showmoreCell a').click();
+        GM_addStyle('.dataTable tbody .oddCell {background-color:rgba(231,239,243,.5)}');
 
-    var top = [],
-        remove = ['realvid.net'],
-        great = ['played.to', 'clicktoview.org'];
-    $('.dataTable tbody tr:not(.sponsorLink) td:first-child > a')
-    .each(function () {
-        var tr = this.parentElement.parentElement;
-        if (great.indexOf(this.innerText) !== -1) {
-            tr.style.backgroundColor = 'rgba(230,0,0,.05)';
-            top.push(tr);
-            tr.remove();
-        } else if (remove.indexOf(this.innerText) !== -1) {
-            tr.remove();
-        }
-    });
-    $($('.dataTable tbody')[0]).prepend(top);
-    $('.dataTable tbody tr:not(.sponsorLink)').each(function (ind) {
-        if (ind >= 10) {
-            $(this).hide()
-        }
-    });
-    document.body.scrollIntoView()
-    $('.dataTable tbody tr:not(.sponsorLink) td:first-child > a')
-    .each(function () {
-        var link = this;
-        $.get(link.href, function (data) {
-            link.href = $(data).find('.linksWrapper .fullyGreenButton')[0].href;
+        var top = [],
+            remove = ['realvid.net'],
+            great = ['played.to', 'clicktoview.org'];
+        $('.dataTable tbody tr:not(.sponsorLink) td:first-child > a')
+        .each(function () {
+            var tr = this.parentElement.parentElement;
+            if (great.indexOf(this.innerText) !== -1) {
+                tr.style.backgroundColor = 'rgba(230,0,0,.05)';
+                top.push(tr);
+                tr.remove();
+            } else if (remove.indexOf(this.innerText) !== -1) {
+                tr.remove();
+            }
         });
-    });
-} else if ($('#frame').length) { // you've come to a player page
-    $('.partnerButton').attr('href', '#').text('Play in fullscreen').removeAttr('target')
-    .click(function (e) {
-        alert("This isn't functional yet. Try again later");
-        return false; // stops default and propagation
-    });
-    // $.get($('a[title="Back to movie page"]')[0].href, function (data) {
-    //     if ($(data).find('#header .menuBoxHeader .active')[0].innerText === "TV SHOWS") {
-    //         var episode = parseInt($('span.season_episode')[0]
-    //             .innerText.replace(/\D/g, ''));
-    //         $.get($(data).find('div.breadcrumb li:last-child a')[0].href, function (d) {
-    //             var found = false,
-    //                 origEpisode = null;
-    //             $(d).find('div.seasonEpisodeList span.epnomber a').each(function () {
-    //                 if (parseInt(this.innerText.replace(/\D/g, '')) + 1 === episode) {
-    //                     $('a[title="Back to link page"]').removeAttr('title')
-    //                     .attr('href', this.href).text('Next Episode');
-    //                     found = true;
-    //                     return false;
-    //                 }
-    //                 if (parseInt(this.innerText.replace(/\D/g, '')) === episode) {
-    //                     origEpisode = this.parentElement.parentElement;
-    //                 }
-    //             });
-    //             if ($(d).find('div.seasonEpisodeList > div:first-child')[0] === origEpisode) {
-    //                 // getting there...
-    //             }
-    //         });
-    //     } else {
-    //         $('a[title="Back to link page"]').remove();
-    //     }
-    // });
+        $($('.dataTable tbody')[0]).prepend(top);
+        $('.dataTable tbody tr:not(.sponsorLink)').each(function (ind) {
+            if (ind >= 10) {
+                $(this).hide()
+            }
+        });
+        document.body.scrollIntoView()
+        $('.dataTable tbody tr:not(.sponsorLink) td:first-child > a')
+        .each(function () {
+            var link = this;
+            $.get(link.href, function (data) {
+                link.href = $(data).find('.linksWrapper .fullyGreenButton')[0].href;
+            });
+        });
+    } else if ($('#frame').length) { // you've come to a player page
+        $('.partnerButton').attr('href', '#').text('Play in fullscreen').removeAttr('target')
+        .click(function (e) {
+            alert("This isn't functional yet. Try again later");
+            $('iframe')[0].contentWindow.postMessage(secret, '*');
+            return false; // stops default and propagation
+        });
+        // $.get($('a[title="Back to movie page"]')[0].href, function (data) {
+        //     if ($(data).find('#header .menuBoxHeader .active')[0].innerText === "TV SHOWS") {
+        //         var episode = parseInt($('span.season_episode')[0]
+        //             .innerText.replace(/\D/g, ''));
+        //         $.get($(data).find('div.breadcrumb li:last-child a')[0].href, function (d) {
+        //             var found = false,
+        //                 origEpisode = null;
+        //             $(d).find('div.seasonEpisodeList span.epnomber a').each(function () {
+        //                 if (parseInt(this.innerText.replace(/\D/g, '')) + 1 === episode) {
+        //                     $('a[title="Back to link page"]').removeAttr('title')
+        //                     .attr('href', this.href).text('Next Episode');
+        //                     found = true;
+        //                     return false;
+        //                 }
+        //                 if (parseInt(this.innerText.replace(/\D/g, '')) === episode) {
+        //                     origEpisode = this.parentElement.parentElement;
+        //                 }
+        //             });
+        //             if ($(d).find('div.seasonEpisodeList > div:first-child')[0] === origEpisode) {
+        //                 // getting there...
+        //             }
+        //         });
+        //     } else {
+        //         $('a[title="Back to link page"]').remove();
+        //     }
+        // });
+    }
+} else {
+    window.addEventListener("message", function (event) {
+        if (event.origin === "http://www.solarmovie.is"
+            && event.data === secret) {
+            // make it fullscreen
+        };
+    }, false);
 }
